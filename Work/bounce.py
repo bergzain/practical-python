4c497d6f6aa303669cd32c87a3fc09c6daec8670
Zain <Zain.hazzouri@gmail.com>
2020-10-13 23:22:42 +0200

    chapter 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
-# bounce.py
+# bounce.py()
 #
 # Exercise 1.5
+hight = 100
+for i in range(1,11):
+    hight = hight * (3.0/5.0)
+    print(i,round(hight,4))
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv,sys
+
+def portfolio_cost(filename):
+    # Your code here
+    purchase_all=0
+
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(f)
+    for row in rows:
+        # row = rows.split(',')
+        shares = int(row[1])
+        price = float(row[2])
+        purchase = shares*price  
+        purchase_all += purchase
+    return purchase_all
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
